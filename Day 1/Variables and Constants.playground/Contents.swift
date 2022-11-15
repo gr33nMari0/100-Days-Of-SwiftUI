cc04cdc4339b59929e4bfe9a4c98ce0feb16e47a
gr33nMari0 <peter2008john@gmail.com>
2022-11-15 23:17:11 +1100

    Completed Day 1 :100:
    First day done :white_check_mark:

---
+++ Day 1/Variables and Constants.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+var greeting = "Hello, playground" // Var defines non-constant variables
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne" // Let defines constants
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
