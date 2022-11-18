0a57f7c0099f067576e9ba186a2a0036fd79482e
gr33nMari0 <peter2008john@gmail.com>
2022-11-18 20:29:13 +1100

    Completed Day 4 🏔

---
+++ Day 4/Checkpoint 2.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+var arrStrings = ["Hey", "there", "this", "is", "a", "word!", "I", "am", "a", "person."]
+print(arrStrings.count)
+var setStrings: Set<String> = Set(arrStrings)
+print(setStrings.count)
